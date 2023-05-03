b9bb9ec384327a37806514cef4dbfb64a92b59f0
Herman van Dompseler <herman.vandompseler@surf.nl>
2023-05-03 17:05:29 +0200

    another way to find challenges

--- pages/api/app/challenges.js
+++ pages/api/app/challenges.js
@@ -1,4 +1,5 @@
-import { getDatasourceEntries } from '../../../lib/storyblok';
+//import { getDatasourceEntries } from '../../../lib/storyblok';
+import { getStoriesByQuery } from '../../../lib/storyblok';
 
 export default async function handler(req, res) {
 
@@ -7,12 +8,15 @@ export default async function handler(req, res) {
 
     // set params
     let params = {};
-    params.datasource = "challenges"
+    //params.datasource = "challenges"
+    params.starts_with = "practice/challenges/";
+    params.is_startpage = 1;
 
     // go
     // console.log("Go", params)
     try {
-        data = await getDatasourceEntries(params);
+        //data = await getDatasourceEntries(params);
+        data = await getStoriesByQuery(params);
     } catch (error) {
         status = 404;
         data.name = error.name;
